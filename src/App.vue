e3479fa38e0f8c5d702a122c2ff08f0b9d9ccc91
selipasha <gipwusa@gmail.com>
2022-04-26 19:19:01 +0500

    modal

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal :header="header" :content="content" theme="dark" @close="toggleModal" />
+  </div>
+  <button @click="toggleModal">Open modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Modal,
+  },
+  data() {
+    return {
+      title: "My Vue App",
+      header: "Sign up for the Giveaway",
+      content: "Hoooray! You are a winner!",
+      showModal: false
+    };
+  },
+  methods: {
+    toggleModal() {
+     this.showModal = !this.showModal
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Avenir, sans-serif;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="backdrop" @click="closeModal">
+    <div class="modal" :class="{ black: theme = 'dark' }">
+      <h1>{{ header }}</h1>
+      <p>{{ content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "content"],
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    }
+  }
+};
+</script>
+
+<style scoped>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+  text-align: center;
+}
+
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+
+.modal h1 {
+  color: #03cfb4;
+  border: none;
+}
+
+.modal p {
+  font-family: normal;
+}
+
+.black {
+  background: #000;
+  color: #fff;
+}
+</style>
